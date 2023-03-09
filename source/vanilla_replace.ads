e3ac0b8de46d291048b8f6daffb6395bb651c6c8
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-09 05:29:39 +0100

    Update and rename vanilla_replacer_v1.ads to vanilla_replace.ads

---
+++ source/vanilla_replace.ads
@@ -0,0 +1,72 @@
+--
+--  (c) 2023 Jesper Quorning, license: MIT OR Apache 2.0
+--  DANMARK
+--
+
+package Vanilla_Replace
+is
+   ----------------
+   -- Characters --
+   ----------------
+   
+   generic
+      type Character_In  is (<>);
+      type Character_Out is (<>);
+
+      with function
+         To_Character_Out (Item : Character_In)
+           return Character_Out;
+
+--   type String_Access is access String;
+      type String (<>)
+        is array (Positive range <>)
+        of Character_Out;
+
+--   type Map_Array is
+--     array (Character)
+--       of String_Access;
+--   Map : Map_Array;
+--   with function Map (Item : Character) return String_Access;
+
+      with function
+         Map (Item : Character_In)
+           return String;
+
+--   New_Line : Character;
+
+      with procedure
+         Put (Item : Character_Out);
+--   with procedure Put (Item : String_Out);
+
+   package Characters_V1
+   is
+      Break : Character_In := Character_In'Last;
+
+      State_Break : Boolean  := False;
+
+--   Map :
+--     array (Character)
+--       of String_Access
+--         := (others => null);
+
+      procedure
+        Process (Item : Character_In);
+
+--   procedure Process (Item : String);
+   --  Convinience.
+
+   end Characters_V1;
+   
+   --------------
+   -- Strings --
+   --------------
+   
+   package Strings_V1
+   is
+   end Strings_V1;
+
+end Vanilla_Replace;
+
+-----------------------------------------------------------------------------
+-----  - - -- = = ==  G U D  B E V A R E  D A N M A R K  == = = -- - -  -----
+-----------------------------------------------------------------------------

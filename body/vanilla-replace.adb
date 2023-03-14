3108fe631a179d444740507fc6d7af474b6b1dee
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-14 02:46:03 +0100

    Create

---
+++ body/vanilla-replace.adb
@@ -0,0 +1,48 @@
+--
+--  (c) 2023 Jesper Quorning, license: MIT OR Apache 2.0
+--  DANMARK
+--
+
+package body Vanilla_Replace
+is
+   -------------
+   -- Process --
+   -------------
+
+   procedure Process (Item : Character_In)
+   is
+   begin
+      if State_Break then
+--         if Map (Item) = null then
+--            raise Program_Error;
+--         else
+            for X in Map (Item)'Range loop
+               Put (Map (Item) (X));
+            end loop;
+--         end if;
+      else
+         if Item = Break then
+            State_Break := True;
+         else
+            Put (To_Character_Out (Item));
+         end if;
+      end if;
+   end Process;
+
+   -------------
+   -- Process --
+   -------------
+
+   -- procedure Process (Item : String)
+   -- is
+   -- begin
+   --    for A in Item'Range loop
+   --       Process (Character (Item (A)));
+   --    end loop;
+   -- end Process;
+
+end Vanilla_Replace;
+
+-----------------------------------------------------------------------------
+-----  - - -- = = ==  G U D  B E V A R E  D A N M A R K  == = = -- - -  -----
+-----------------------------------------------------------------------------

3a838cfe62319abc41946fc0e6cd43b74dc6eb25
Jesper Quorning <jesper.quorning@gmail.com>
2023-05-10 13:32:18 +0200

    Update vanilla_replace.ads

--- source/vanilla_replace.ads
+++ source/vanilla_replace.ads
@@ -1,64 +1,39 @@
 --
 --  (c) 2023 Jesper Quorning, license: MIT OR Apache 2.0
---  DANMARK
 --
 
 package Vanilla_Replace
 is
-   ----------------
-   -- Characters --
-   ----------------
+   --------------------
+   -- String of Item --
+   --------------------
 
    generic
-      type Character_In  is (<>);
-      type Character_Out is (<>);
+      type Item_In  is (<>);
+      type Item_Out is (<>);
 
       with function
-         To_Character_Out (Item : Character_In)
-           return Character_Out;
+         To_Item_Out (Item : Item_In)
+           return Item_Out;
 
---   type String_Access is access String;
-      type String (<>)
+      type Item_String (<>)
         is array (Positive range <>)
-        of Character_Out;
+        of Item_Out;
 
---   type Map_Array is
---     array (Character)
---       of String_Access;
---   Map : Map_Array;
---   with function Map (Item : Character) return String_Access;
+      with function Map (Item : Item_In)
+           return Item_String;
 
-      with function
-         Map (Item : Character_In)
-           return String;
+      with procedure Put (Item : Item_Out);
 
---   New_Line : Character;
-
-      with procedure
-         Put (Item : Character_Out);
---   with procedure Put (Item : String_Out);
-
-   package Characters
+   package Items_V1
    is
-      Break : Character_In := Character_In'Last;
+      Break : Item_In := Item_In'Last;
 
       State_Break : Boolean  := False;
 
---   Map :
---     array (Character)
---       of String_Access
---         := (others => null);
+      procedure Process (Item : Item_In);
+      procedure Process (Item : Item_String);
 
-      procedure
-        Process (Item : Character_In);
-
---   procedure Process (Item : String);
-   --  Convinience.
-
-   end Characters;
+   end Items_V1;
 
 end Vanilla_Replace;
-
------------------------------------------------------------------------------
------  - - -- = = ==  G U D  B E V A R E  D A N M A R K  == = = -- - -  -----
------------------------------------------------------------------------------

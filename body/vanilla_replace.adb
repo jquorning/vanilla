622edde68234fbf6ed4d58362a605f663b5d5869
Jesper Quorning <jesper.quorning@gmail.com>
2023-05-10 13:39:00 +0200

    Update vanilla_replace.adb

--- body/vanilla_replace.adb
+++ body/vanilla_replace.adb
@@ -1,49 +1,49 @@
 --
 --  (c) 2023 Jesper Quorning, license: MIT OR Apache 2.0
---  DANMARK
 --
 
 package body Vanilla_Replace
 is
-   package body Characters
+   package body Items_V1
    is
       -------------
       -- Process --
       -------------
 
-      procedure Process (Item : Character_In)
+      procedure Process (Item : Item_In)
       is
       begin
          if State_Break then
---         if Map (Item) = null then
---            raise Program_Error;
---         else
-               for X in Map (Item)'Range loop
-                  Put (Map (Item) (X));
+            declare
+               S : Item_String renames Map (Item);
+            begin
+               for I in Map (Item)'Range loop
+                  Put (S (I));
                end loop;
---         end if;
+            end;
+            State_Break := False;
          else
             if Item = Break then
                State_Break := True;
             else
-               Put (To_Character_Out (Item));
+               Put (To_Item_Out (Item));
             end if;
          end if;
       end Process;
 
-   end Characters;
+      -------------
+      -- Process --
+      -------------
 
-   -------------
-   -- Process --
-   -------------
+      procedure Process (Item : String)
+      is
+      begin
+         for A in Item'Range loop
+            Process (Item (A));
+         end loop;
+      end Process;
 
-   --  procedure Process (Item : String)
-   --  is
-   --  begin
-   --    for A in Item'Range loop
-   --       Process (Character (Item (A)));
-   --    end loop;
-   --  end Process;
+   end Items;
 
 end Vanilla_Replace;
 

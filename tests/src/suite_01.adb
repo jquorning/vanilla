55fe2cdd842c661a741288ab6b664aaceb19fa0c
Jesper Quorning <jesper.quorning@gmail.com>
2023-09-08 17:21:26 +0200

    Add tests for Translate_V1/V2

--- tests/src/suite_01.adb
+++ tests/src/suite_01.adb
@@ -1,11 +1,18 @@
 with Test_Substitutes;
+with Test_Translate_V1;
+with Test_Translate_V2;
+
 with AUnit.Test_Caller;
 
 package body Suite_01
 is
    use AUnit.Test_Suites;
+   package Trv1 renames Test_Translate_V1;
+   package Trv2 renames Test_Translate_V2;
 
-   package Caller is new AUnit.Test_Caller (Test_Substitutes.Test);
+   package Caller  is new AUnit.Test_Caller (Test_Substitutes.Test);
+   package Caller1 is new AUnit.Test_Caller (Trv1.Fixture);
+   package Caller2 is new AUnit.Test_Caller (Trv2.Fixture);
 
    function Suite return Access_Test_Suite
    is
@@ -22,6 +29,14 @@ is
       Ret.Add_Test (Caller.Create ("Item 07", Item_07'Access));
       Ret.Add_Test (Caller.Create ("Item 08", Item_08'Access));
       Ret.Add_Test (Caller.Create ("Item 09", Item_09'Access));
+
+      Ret.Add_Test (Caller1.Create ("Tr_V1 01", Trv1.Afprovning_01'Access));
+      Ret.Add_Test (Caller1.Create ("Tr_V1 02", Trv1.Afprovning_02'Access));
+      Ret.Add_Test (Caller1.Create ("Tr_V1 03", Trv1.Afprovning_03'Access));
+
+      Ret.Add_Test (Caller2.Create ("Tr_V2 01", Trv2.Afprovning_01'Access));
+      Ret.Add_Test (Caller2.Create ("Tr_V2 02", Trv2.Afprovning_02'Access));
+      Ret.Add_Test (Caller2.Create ("Tr_V2 03", Trv2.Afprovning_03'Access));
 
       return Ret;
    end Suite;
